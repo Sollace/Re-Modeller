9ad2508827f8de7e8145da756fc083d71717a0a4
Sollace <sollacea@gmail.com>
2020-09-01 10:47:45 +0200

    Fixed model resource reloading not reading from the list of registered rewriters

--- src/com/minelittlepony/remodeller/LiteModReModeller.java
+++ src/com/minelittlepony/remodeller/LiteModReModeller.java
@@ -2,6 +2,9 @@ package com.minelittlepony.remodeller;
 
 import java.io.File;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.mumfrey.liteloader.LiteMod;
 import com.mumfrey.liteloader.core.LiteLoader;
 
@@ -10,25 +13,27 @@ import net.minecraft.client.resources.IReloadableResourceManager;
 
 public class LiteModReModeller implements LiteMod {
 
-	@Override
-	public String getName() {
-		return "@NAME@";
-	}
+    public static final Logger LOGGER = LogManager.getLogger();
 
-	@Override
-	public String getVersion() {
-		return "@VERSION@";
-	}
+    @Override
+    public String getName() {
+        return "@NAME@";
+    }
 
-	@Override
-	public void init(File configPath) {
-	    ((IReloadableResourceManager)Minecraft.getMinecraft().getResourceManager()).registerReloadListener(ModelRewriteManager.INSTANCE);
+    @Override
+    public String getVersion() {
+        return "@VERSION@";
+    }
 
-	    LiteLoader.getInstance().registerExposable(ModelRewriteManager.INSTANCE, null);
-	}
+    @Override
+    public void init(File configPath) {
+        ((IReloadableResourceManager)Minecraft.getMinecraft().getResourceManager()).registerReloadListener(ModelRewriteManager.INSTANCE);
 
-	@Override
-	public void upgradeSettings(String version, File configPath, File oldConfigPath) {
+        LiteLoader.getInstance().registerExposable(ModelRewriteManager.INSTANCE, null);
+    }
 
-	}
+    @Override
+    public void upgradeSettings(String version, File configPath, File oldConfigPath) {
+
+    }
 }
--- src/com/minelittlepony/remodeller/ModelResourceReloader.java
+++ src/com/minelittlepony/remodeller/ModelResourceReloader.java
@@ -3,10 +3,10 @@ package com.minelittlepony.remodeller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 
@@ -18,49 +18,41 @@ import net.minecraft.util.ResourceLocation;
 
 abstract class ModelResourceReloader implements IResourceManagerReloadListener {
 
-    private static final List<String> EMPTY = new ArrayList<>();
-
     protected final Map<String, RenderConfig> rewrites = new HashMap<String, RenderConfig>();
-
-    private List<String> loadLines(InputStream stream) {
-        try {
-            return IOUtils.readLines(stream, Charset.defaultCharset());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            IOUtils.closeQuietly(stream);
-        }
-
-        return EMPTY;
-    }
 
     @Override
     public void onResourceManagerReload(IResourceManager manager) {
         rewrites.clear();
 
-        List<String> rewritersToLoad = new ArrayList<String>();
+        Set<String> rewritersToLoad = new HashSet<String>();
 
         for (String domain : manager.getResourceDomains()) {
             try {
                 for (IResource resource : manager.getAllResources(new ResourceLocation(domain, "modelrewrites/modelrewriters.txt"))) {
-                    for (String i : loadLines(resource.getInputStream())) {
-                        if (rewritersToLoad.contains(i)) rewritersToLoad.add(i);
+                    try (InputStream s = resource.getInputStream()) {
+                        rewritersToLoad.addAll(IOUtils.readLines(s, Charset.defaultCharset()));
                     }
                 }
-            } catch (IOException e) { }
+            } catch (IOException e) {
+                LiteModReModeller.LOGGER.error("Could not load modelrewriters.txt", e);
+            }
         }
 
         for (String domain : manager.getResourceDomains()) {
             try {
                 for (String className : rewritersToLoad) {
                     for (IResource resource : manager.getAllResources(new ResourceLocation(domain, String.format("modelrewrites/%s.json", className)))) {
-                        RenderConfig obj = RenderConfig.fromJson(resource.getInputStream());
-                        if (obj != null) {
-                            rewrites.put(className, obj);
+                        try (InputStream s = resource.getInputStream()) {
+                            RenderConfig obj = RenderConfig.fromJson(s);
+                            if (obj != null) {
+                                rewrites.put(className, obj);
+                            }
                         }
                     }
                 }
-            } catch (IOException e) {  }
+            } catch (IOException e) {
+                LiteModReModeller.LOGGER.error("Could not load modelrewriter", e);
+            }
         }
     }
 }
